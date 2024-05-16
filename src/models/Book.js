af0e728869cbdf3b59686d0d0ee15a8c282f9885
Diego Bottegoni <146596468+DiegoBottegoni@users.noreply.github.com>
2024-05-16 14:11:54 -0300

    Update Book.js

--- src/models/Book.js
+++ src/models/Book.js
@@ -1,11 +1,6 @@
 // Represents a book in the bookstore.
 class Book {
   // Create a new Book instance.
-  // @param {string} title - The title of the book.
-  // @param {string} author - The author of the book.
-  // @param {string} isbn - The ISBN of the book.
-  // @param {number} price - The price of the book.
-  // @param {boolean} availability - The availability of the book.
   constructor(title, author, isbn, price, availability) {
     this.title = title;
     this.author = author;
@@ -13,6 +8,34 @@ class Book {
     this.price = price;
     this.availability = availability;
   }
+
+  // Define how to serialize the Book instance to JSON
+  toJSON() {
+    return {
+      title: this.title,
+      author: this.author,
+      isbn: this.isbn,
+      price: this.price,
+      availability: this.availability
+    };
+  }
 }
 
-module.exports = { Book };
+// Represents an eBook, subclass of Book
+class EBook extends Book {
+  // Create a new EBook instance.
+   constructor(title, author, isbn, price, availability, fileSize) {
+    super(title, author, isbn, price, availability);
+    this.fileSize = fileSize;
+  }
+
+  // Define how to serialize the EBook instance to JSON
+  toJSON() {
+    return {
+      ...super.toJSON(),
+      fileSize: this.fileSize
+    };
+  }
+}
+
+module.exports = { Book, EBook };
