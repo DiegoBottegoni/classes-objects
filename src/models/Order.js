f4ce8afd26f52ef048a8b3a04706be9f82195e4f
Diego Bottegoni <146596468+DiegoBottegoni@users.noreply.github.com>
2024-05-17 15:34:19 -0300

    Update Order.js

--- src/models/Order.js
+++ src/models/Order.js
@@ -1,3 +1,5 @@
+const { calculateTotalPrice } = require('../utils/utils.js');
+
 // Represents an order made by a user in the bookstore.
 class Order {
   // Create a new Order instance.
@@ -6,14 +8,10 @@ class Order {
     this.books = books;
     this.totalPrice = this.calculateTotalPrice();
   }
-
+  
   // Calculate the total price of the order.
-  // Only include the price of books that are available.
-  // Returns {number} The total price of the order.
   calculateTotalPrice() {
-    return this.books.reduce((total, book) => {
-      return book.availability ? total + book.price : total;
-    }, 0);
+    return calculateTotalPrice(this.books);
   }
 
   // Define how to serialize the Order instance to JSON
